976b817c8a07215e4c41d90b0fad690c6ef9ccde
isabellanogueirasilva <159037643+isabellanogueirasilva@users.noreply.github.com>
2024-04-24 20:59:27 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+document.getElementById('signup-form').addEventListener('submit', function(event) {
+    event.preventDefault(); // Impede o envio do formulário padrão
+  
+    // Aqui você pode adicionar a lógica para processar os dados do formulário, como enviar para um servidor, salvar em um banco de dados, etc.
+  
+    alert('Cadastro realizado com sucesso! Estamos feliz por ser um dos nossos clientes fiel'); // Exemplo simples de alerta
+  });
+  
+
